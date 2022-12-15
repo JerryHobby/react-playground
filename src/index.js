14b462d6c84298f20f040927ab7806626fd8239f
Jerry Hobby <Jerry@JerryHobby.com>
2022-12-14 19:13:49 -0600

    initial - tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+//import registerServiceWorker from "./registerServiceWorker";
+//import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Counters from "./components/counters";
+
+let root = document.getElementById("root");
+root = createRoot(root);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Counters />
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
