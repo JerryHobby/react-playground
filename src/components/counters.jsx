9d89b6e7ebc4169d012ca17e188e2eff568637ab
Jerry Hobby <Jerry@JerryHobby.com>
2023-01-04 16:57:40 -0600

    add navbar features

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,66 +1,35 @@
 import React, { Component } from 'react';
 import Counter from './counter';
+import './counters.css';
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 10, selected: true, label: 'First' },
-      { id: 2, value: 20, selected: true, label: 'Second' },
-      { id: 3, value: 30, selected: true, label: 'Third' },
-      { id: 4, value: 50, selected: true, label: 'Fourth' },
-    ],
-  };
+  constructor(props) {
+    super(props);
+    console.log('Counters Constructor: ', this.props);
+  }
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-
-    this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-
-    if (counters[index].value > 0) {
-      counters[index].value--;
-    }
-
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-    });
-    this.counters = counters;
-    this.setState({});
-  };
+  componentDidMount() {
+    // make Ajax calls to get data
+    console.log('Counters Mounted');
+  }
 
   render() {
+    const { counters, onReset, onDelete, onIncrement, onDecrement } =
+      this.props;
+    console.log('Counters Rendered');
+
     return (
       <React.Fragment>
-        <button
-          onClick={this.handleReset}
-          className="btn btn-primary btn-lg m-8"
-        >
+        <button onClick={onReset} className="btn btn-primary btn-lg m-8">
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {counters.map((counter) => (
           <div key={counter.id}>
             <Counter
               key={counter.id}
-              onDelete={this.handleDelete}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
+              onDelete={onDelete}
+              onIncrement={onIncrement}
+              onDecrement={onDecrement}
               counter={counter}
             />
           </div>
