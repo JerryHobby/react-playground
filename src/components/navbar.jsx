0927ceb480344e5a28df4e187128499bb8fd7842
Jerry Hobby <Jerry@JerryHobby.com>
2023-01-08 17:06:35 -0600

    update navbar with reload button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -15,18 +15,41 @@ class NavBar extends Component {
     console.log('NavBar Rendered');
 
     return (
-      <nav className="navbar navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Home{' '}
-          {/*
-          <span className="badge badge-pill bg-primary">
-            Counters: {totalCounters}
-          </span>{' '}
-          <span className="badge badge-pill bg-secondary">
-            Items: {totalItems}
-          </span>{' '}
-    */}
+          Navbar
         </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+            <a
+              className="nav-item nav-link"
+              href="#"
+              onClick={() => window.location.reload()}
+            >
+              Reload
+            </a>
+            <a className="nav-item nav-link" href="#">
+              Unused
+            </a>
+            <a className="nav-item nav-link disabled" href="#">
+              Disabled
+            </a>
+          </div>
+        </div>
       </nav>
     );
   }
