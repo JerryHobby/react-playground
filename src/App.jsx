a9ab8767a32ee13108f9504b9528466128d517b5
Jerry Hobby <Jerry@JerryHobby.com>
2023-01-12 18:58:48 +0000

    remove counters.jsx component from app.jsx and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import NavBar from './components/navbar';
-import Counters from './components/counters';
 import { getGenres } from './services/fakeGenreService';
 import { getMovies, deleteMovie } from './services/fakeMovieService';
 import Movies from './components/movies';
@@ -13,12 +12,6 @@ class App extends Component {
     pageSize: 4,
     currentPage: 1,
     currentGenre: 'all',
-    counters: [
-      { id: 1, value: 10, selected: true, label: 'First' },
-      { id: 2, value: 20, selected: true, label: 'Second' },
-      { id: 3, value: 30, selected: true, label: 'Third' },
-      { id: 4, value: 50, selected: true, label: 'Fourth' },
-    ],
   };
 
   constructor(props) {
@@ -37,54 +30,14 @@ class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {}
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-
-    this.setState({ counters });
-  };
-
-  handleDecrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-
-    if (counters[index].value > 0) {
-      counters[index].value--;
-    }
-
-    this.setState({ counters });
-  };
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-    });
-    this.counters = counters;
-    this.setState({});
-  };
-
   render() {
     const { counters } = this.state;
     console.log('App Rendered ***********');
     let totalItems = 0;
 
-    for (var i = 0; i < counters.length; i++) {
-      totalItems += counters[i].value;
-    }
-    // console.log('Total Items: ', totalItems);
     return (
       <React.Fragment>
         <NavBar
-          totalItems={totalItems}
-          totalCounters={counters.filter((c) => c.value > 0).length}
         />
         <main className="container">
           <Movies
@@ -98,20 +51,11 @@ class App extends Component {
             onPageChange={this.handlePageChange}
             onGenreChange={this.handleGenreChange}
           />
-          {/*
-          <hr></hr>
-          <Counters
-            counters={counters}
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onDecrement={this.handleDecrement}
-          />
-          */}
         </main>
       </React.Fragment>
     );
   }
+  
   handlePageChange = (page) => {
     this.setState({ currentPage: page.page });
   };
